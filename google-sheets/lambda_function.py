bdc2c143e32241c7ad45c7d744b10d16bb45bf49
Jhon Jairo Rojas Ortiz <jhonjairorojasortiz@gmail.com>
2023-05-09 13:21:58 -0500

    actualizacion de datos de ultima fecha si es que existe

--- google-sheets/lambda_function.py
+++ google-sheets/lambda_function.py
@@ -1,18 +1,65 @@
-from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
 from google.oauth2 import service_account
+import gspread
 import pandas as pd
 from datetime import date
-import json
+import requests
+from icecream import ic
 
 scopes = ["https://www.googleapis.com/auth/spreadsheets"]
-key = "data/google-sheets\data\ordinal-rig-386013-f4be285ee861.json"
+key = "key/service_account.json"
 spreadsheet_id = "1vB9JB_vxYKKuRlRQr1CckKdx7MhlVcFDgBPigEF50oY"
-creds = None
 creds = service_account.Credentials.from_service_account_file(key, scopes=scopes)
 service = build("sheets", "v4", credentials=creds)
 sheet = service.spreadsheets()
-tc = 3.76
+url = "https://api.apis.net.pe/v1/tipo-cambio-sunat"
+today = date.today().strftime("%Y-%m-%d")
+params = {"fecha": today}
+r = requests.get(url, params)
+tc_compra = r.json().get("compra")
+tc_venta = r.json().get("venta")
+ic(tc_venta, tc_compra)
+sa = gspread.service_account(key)
+sh = sa.open("Contabilidad personal")
+wks = sh.worksheet("Response")
+
+
+def formatFloat(obj):
+    if isinstance(obj, float):
+        return "{:,.2f}".format(obj)
+    return repr(obj)
+
+
+ic.configureOutput(argToStringFunction=formatFloat)
+
+
+def get_values_sheet(range):
+    data = sheet.values().get(spreadsheetId=spreadsheet_id, range=range).execute()
+    values = data.get("values", [])
+    return values
+
+
+def get_final_date_index():
+    range = "Response!A:A"
+    arr_date = get_values_sheet(range)
+
+    if arr_date:
+        date = arr_date[-1]
+        final_date = (
+            date[0]
+            if isinstance(date, list)
+            and date
+            and isinstance(date[0], str)
+            and date[0] != "Fecha"
+            else None
+        )
+        columns_numbers = len(arr_date)
+        ic(final_date, columns_numbers)
+        return final_date, columns_numbers
+    return None
+
+
+final_date, index = get_final_date_index()
 
 
 def lambda_handler(event=None, context=None):
@@ -23,7 +70,8 @@ def lambda_handler(event=None, context=None):
         "Empresas (dólares)!A:B",
         "Cuentas bancarias (soles)!A:B",
         "Cuentas bancarias (dólares)!A:B",
-        "Consumo TC!A:B",
+        "Consumo TC (soles)!A:B",
+        "Consumo TC (dólares)!A:B",
         "Metas (soles)!A:B",
         "Metas (dólares)!A:B",
     ]
@@ -34,7 +82,8 @@ def lambda_handler(event=None, context=None):
         empresas_dolares,
         cuentas_bancaria_soles,
         cuentas_bancaria_dolares,
-        consumo_tc,
+        consumo_tc_soles,
+        consumo_tc_dolares,
         meta_soles,
         meta_dolares,
     ) = [get_values_sheet(x) for x in ranges]
@@ -45,56 +94,72 @@ def lambda_handler(event=None, context=None):
         empresas_dolares,
         cuentas_bancaria_soles,
         cuentas_bancaria_dolares,
-        consumo_tc,
+        consumo_tc_soles,
+        consumo_tc_dolares,
         meta_soles,
         meta_dolares,
     ]
 
     (
-        monto_deudores,
+        monto_deudores_soles,
         monto_debo,
         monto_empresas_soles,
         monto_empresas_dolares,
         monto_cuentas_bancaria_soles,
         monto_cuentas_bancaria_dolares,
-        monto_consumo_tc,
+        monto_consumo_tc_soles,
+        monto_consumo_tc_dolares,
         monto_metas_soles,
         monto_meta_dolares,
     ) = [get_total(x) for x in montos]
+
     process_data(
-        monto_deudores,
+        monto_deudores_soles,
         monto_debo,
         monto_empresas_soles,
         monto_empresas_dolares,
         monto_cuentas_bancaria_soles,
         monto_cuentas_bancaria_dolares,
-        monto_consumo_tc,
+        monto_consumo_tc_soles,
+        monto_consumo_tc_dolares,
         monto_metas_soles,
         monto_meta_dolares,
     )
-    return {"statusCode": 200, "body": json.dumps("Success!")}
+
+    return "Success"
+
+    # return {"statusCode": 200, "body": json.dumps("Success!")}
 
 
 def process_data(
-    monto_deudores,
+    monto_deudores_soles,
     monto_debo,
     monto_empresas_soles,
     monto_empresas_dolares,
     monto_cuentas_bancaria_soles,
     monto_cuentas_bancaria_dolares,
-    monto_consumo_tc,
+    monto_consumo_tc_soles,
+    monto_consumo_tc_dolares,
     monto_metas_soles,
     monto_meta_dolares,
 ):
-    today = date.today().strftime("%d/%m/%Y")
-    me_deben = monto_deudores + monto_empresas_soles + tc * monto_empresas_dolares
-    en_cuenta = monto_cuentas_bancaria_dolares * tc + monto_cuentas_bancaria_soles
+    # * Soles
+    me_deben = (
+        monto_deudores_soles + monto_empresas_soles + tc_compra * monto_empresas_dolares
+    )
+
+    en_cuenta = (
+        monto_cuentas_bancaria_dolares * tc_compra + monto_cuentas_bancaria_soles
+    )
     total_positivo = en_cuenta + me_deben
-    total_negativo = monto_consumo_tc + monto_debo
+    total_negativo = (
+        monto_consumo_tc_soles + monto_consumo_tc_dolares * tc_compra + monto_debo
+    )
     patrimonio = total_positivo - total_negativo
-    monto_metas = monto_metas_soles + tc * monto_meta_dolares
+    monto_metas = monto_metas_soles + tc_compra * monto_meta_dolares
     falta_meta = monto_metas - patrimonio if monto_metas else 0
-    data = [
+
+    data_soles = [
         me_deben,
         en_cuenta,
         total_positivo,
@@ -103,21 +168,66 @@ def process_data(
         monto_metas,
         falta_meta,
     ]
-    print(data)
+    # ic.configureOutput(prefix=f"Soles {' ' * 3}|")
+    ic(
+        me_deben,
+        en_cuenta,
+        total_positivo,
+        total_negativo,
+        patrimonio,
+        monto_metas,
+        falta_meta,
+    )
+
+    # * Dólares
+    me_deben = (
+        monto_deudores_soles + monto_empresas_soles
+    ) / tc_venta + monto_empresas_dolares
+
+    en_cuenta = monto_cuentas_bancaria_dolares + monto_cuentas_bancaria_soles / tc_venta
+    total_positivo = en_cuenta + me_deben
+    total_negativo = (
+        monto_consumo_tc_soles + monto_debo
+    ) / tc_venta + monto_consumo_tc_dolares
+    patrimonio = total_positivo - total_negativo
+    monto_metas = monto_metas_soles / tc_venta + monto_meta_dolares
+    falta_meta = monto_metas - patrimonio if monto_metas else 0
+
+    data_dolares = [
+        me_deben,
+        en_cuenta,
+        total_positivo,
+        total_negativo,
+        patrimonio,
+        monto_metas,
+        falta_meta,
+    ]
+    # ic.configureOutput(prefix=f"Dólares {' '* 1}|")
+    ic(
+        me_deben,
+        en_cuenta,
+        total_positivo,
+        total_negativo,
+        patrimonio,
+        monto_metas,
+        falta_meta,
+    )
+    today = date.today().strftime("%d/%m/%Y")
     data_format = (
         [today]
-        + [x for x in data]
-        + [(x / tc) for x in data]
+        + [x for x in data_soles]
+        + [tc_compra, tc_venta]
+        + [(x) for x in data_dolares]
         # + [" S/ " + "{:,.2f}".format(x) for x in data]
-        # + ["$ " + "{:,.2f}".format(x / tc) for x in data]
+        # + ["$ " + "{:,.2f}".format(x / tc_compra) for x in data]
     )
+
+    if (
+        today == final_date and index > 2
+    ):  # * 2 unidades para omitir en caso se trate de la cabecera
+        ic(index)
+        wks.delete_rows(index)
     update_values_sheets("Response!A:Q", [data_format])
-
-
-def get_values_sheet(range):
-    data = sheet.values().get(spreadsheetId=spreadsheet_id, range=range).execute()
-    values = data.get("values", [])
-    return values
 
 
 def update_values_sheets(range, values):
@@ -131,8 +241,13 @@ def update_values_sheets(range, values):
 
 def get_total(array_of_arrays):
     df = pd.DataFrame(array_of_arrays, columns=["Nombre", "Monto"])
-    total = df["Monto"].astype("float64").sum()
+    total = df["Monto"].apply(convert_currency).sum()
     return total
+
+
+def convert_currency(val):
+    new_val = val.replace(",", "").replace("$", "")
+    return float(new_val)
 
 
 lambda_handler()
